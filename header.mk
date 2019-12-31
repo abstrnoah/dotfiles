3fab035f30048d8e36d7c1032591f184143a2086
Noah D. Ortiz <analyticalnoa@gmail.com>
2019-12-31 06:10:49 -0700

    git/makefile uninstall working.

--- header.mk
+++ header.mk
@@ -12,7 +12,9 @@ make_link = $(link_cmd) $(call fullpath,$<) "$@"
 get_shell = $$( awk -F: '$$1=="'"$$USER"'" {print $$7}' /etc/passwd )
 
 nix_env_i_cmd = nix-env --install --file "$<" --attr "$(1)"
+nix_env_e_cmd = nix-env --uninstall
 install_nix_pkg = git -C "$<" checkout "$(1)" && $(call nix_env_i_cmd,$(2))
+uninstall_nix_pkg = $(nix_env_e_cmd)
 
 # $(ANENV_NIXPKGS):
 #	git clone $(ANENV_MASTERREMOTE)/nixpkgs
