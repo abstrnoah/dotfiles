1897995102e3e9de8471fc42583cd78543484a45
Noah D. Ortiz <abstractednoah@brumal.org>
2021-04-08 11:45:04 -0700

    nix/config.nix: declare zoom (for which we need allowUnfreePredicate).

--- package/nix/config.nix
+++ package/nix/config.nix
@@ -1,39 +1,44 @@
 {
-    pulseaudio = true;
-    packageOverrides = pkgs: with pkgs; {
-        desktopPackages = pkgs.buildEnv {
-            name = "desktopPackages";
-            paths = [
-                maven
-                ctags
-                git
-                glibcLocales
-                htop
-                jabref
-                jq
-                nodejs
-                okular
-                jdk
-                pfetch
-                php
-                python3
-                ranger
-                rlwrap
-                tectonic
-                textql
-                tmux
-                tmuxinator
-                toilet
-                vimHugeX
-                visidata
-                xrandr-invert-colors
-                zsh
-                nix-zsh-completions
-                zathura
-            ];
-            pathsToLink = [ "/share" "/bin" "/lib" ];
-            # TODO: fix PATH and MANPATH so that these are available to `man`.
-            extraOutputsToInstall = [ "man" "doc" ];
-        };
+  pulseaudio = true;
+  allowUnfreePredicate = pkg: with (import <an_nixpkgs>) {};
+  builtins.elem (lib.getName pkg) [
+    "zoom"
+  ];
+  packageOverrides = pkgs: with pkgs; {
+    desktopPackages = pkgs.buildEnv {
+      name = "desktopPackages";
+      paths = [
+        maven
+        ctags
+        git
+        glibcLocales
+        htop
+        jabref
+        jq
+        nodejs
+        okular
+        jdk
+        pfetch
+        php
+        python3
+        ranger
+        rlwrap
+        tectonic
+        textql
+        tmux
+        tmuxinator
+        toilet
+        vimHugeX
+        visidata
+        xrandr-invert-colors
+        zsh
+        nix-zsh-completions
+        zathura
+        zoom-us
+      ];
+      pathsToLink = [ "/share" "/bin" "/lib" ];
+      extraOutputsToInstall = [ "man" "doc" ];
     };
+  };
 }
+# TODO: fix PATH and MANPATH so that docs are available to `man`.
