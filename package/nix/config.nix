ba4b19fe35e0a4118a36be5c79c5ab89bfb43ae8
Noah D. Ortiz <abstractednoah@brumal.org>
2021-04-08 02:47:55 -0700

    [machine02-setup] nix/config.nix: declare desktopPackages.

--- package/nix/config.nix
+++ package/nix/config.nix
@@ -1,1 +1,36 @@
-{ pulseaudio = true; }
+{
+    pulseaudio = true;
+    packageOverrides = pkgs: with pkgs; {
+        desktopPackages = pkgs.buildEnv {
+            name = "desktopPackages";
+            paths = [
+                maven
+                ctags
+                git
+                glibc
+                htop
+                jabref
+                jq
+                nodejs
+                okular
+                jdk
+                pfetch
+                php
+                python3
+                ranger
+                rlwrap
+                tectonic
+                textql
+                tmux
+                tmuxinator
+                toilet
+                vimHugeX
+                visidata
+                xrandr-invert-colors
+                zsh
+            ];
+            pathsToLink = [ "/share/man" "/share/doc" "/bin" ];
+            extraOutputsToInstall = [ "man" "doc" ];
+        };
+    };
+}
