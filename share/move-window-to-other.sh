0faaaa194738aa3e01c81e08f70e1fac0a739823
Noah D. Ortiz <abstractednoah@brumal.org>
2021-09-09 13:15:27 -0700

    i3,rofi: "empty" -> "-"; rm more --release.

--- share/move-window-to-other.sh
+++ share/move-window-to-other.sh
@@ -11,13 +11,13 @@ function gen_workspaces()
     i3-msg -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g' | sort -n
 }
 
-WORKSPACE=$( (echo empty; gen_workspaces)  | "${_rofi}" -dmenu -p "move to ws")
+WORKSPACE=$( (echo "-"; gen_workspaces)  | "${_rofi}" -dmenu -p "move to ws")
 
 MAX_DESKTOPS=20
 
 WORKSPACES=$(seq -s '\n' 1 1 "${MAX_DESKTOPS}")
 
-if [ x"empty" = x"${WORKSPACE}" ]
+if [ x"-" = x"${WORKSPACE}" ]
 then
     WORKSPACE=$( (i3-msg -t get_workspaces | tr ',' '\n' | grep num | awk -F: '{print int($2)}' ; \
             echo -e "${WORKSPACES}" ) | sort -n | uniq -u | head -n 1)
