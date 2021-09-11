58779a58e0f3a31dc608425d522d3d789dbe1218
Noah D. Ortiz <abstractednoah@brumal.org>
2021-09-11 10:02:21 -0700

    share/rofi.sh: hacky patch for qutebrowser.

--- share/rofi.sh
+++ share/rofi.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# This is a hacky quick fix. TODO: Somehow rofi needs to run in the presence of
+# zshenv, but sourcing it directly in here doesn't seem to work, plus that's
+# also hacky.
 export LOCALE_ARCHIVE="$(readlink ~/.nix-profile/lib/locale)/locale-archive"
+export QT_XCB_GL_INTEGRATION=none
+
 ~/.nix-profile/bin/rofi "${@}"
