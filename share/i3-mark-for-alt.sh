ceee0f92d0ee8ba6d3484014fb3cfffcd648e548
Noah D. Ortiz <abstractednoah@brumal.org>
2021-11-19 21:28:38 -0800

    i3-mark-for-alt.sh: fix clear_marks case.

--- share/i3-mark-for-alt.sh
+++ share/i3-mark-for-alt.sh
@@ -2,6 +2,11 @@
 
 # set -x
 
+count_marks() {
+    i3-msg -t get_marks \
+        | jq 'map(match("^_alt[01]$"))|length' -r
+}
+
 get_last_mark() {
     i3-msg -t get_marks \
         | jq 'map(match("^_alt[01]$"))|map(.string)|sort|last(.[])' -r
@@ -19,16 +24,20 @@ clear_marks() {
     i3-msg unmark _alt1
 }
 
-
-case $(get_last_mark) in
-    _alt0)
-        mark 1
-        ;;
-    _alt1)
-        mark 0
-        ;;
-    *)
-        clear_marks
-        mark 0
-        ;;
-esac
+if [ $(count_marks) -ge 2 ]; then
+    clear_marks
+    mark 0
+else
+    case $(get_last_mark) in
+        _alt0)
+            mark 1
+            ;;
+        _alt1)
+            mark 0
+            ;;
+        *)
+            clear_marks
+            mark 0
+            ;;
+    esac
+fi
