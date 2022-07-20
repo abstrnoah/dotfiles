9db1222b5f4406eca060ff51bf6f1c2a0a1581b7
Noah D. Ortiz <abstractednoah@brumal.org>
2022-07-20 13:43:31 -0700

    DisableWakeupOnLidOpen: improve, link to hibernate

--- package/acpi/disable_wakeup_on_lid_open.sh
+++ package/acpi/disable_wakeup_on_lid_open.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+# set -x
+
 # Helper script to disable wakeup on lid open on Ubuntu with acpi installed
 # (implmeneted via systemd service).
 #
-# Author: abstractednoah@brumal.org Path:
-# /usr/share/brumal-utils/disable_wakeup_on_lid_open.sh
+# Author: abstractednoah@brumal.org
+# Path: /usr/share/brumal-utils/disable_wakeup_on_lid_open.sh
 
-path=/proc/acpi/wakeup
+_path=/proc/acpi/wakeup
 
 # Get status of LID setting, we expect "*enabled" or "*disabled".
 get_status() {
-    echo $(awk '{if ($1 == "LID") print $3;}' "${path}")
+    awk '{if ($1 == "LID") print $3;}' "${_path}"
 }
 
 # Abort with error if path missing.
-[ -f "${path}" ] || exit 1
+[ -f "${_path}" ] || exit 2
 
 # Disable wakeup on lid open if enabled.
-[ $(get_status) = "*enabled" ] && echo "LID" > ${path}
+[ $(get_status) = "*disabled" ] || echo "LID" > ${_path}
 
 # Check that disabled now.
-[ $(get_status) = "*enabled" ] && exit 1
+[ $(get_status) = "*disabled" ] || exit 3
