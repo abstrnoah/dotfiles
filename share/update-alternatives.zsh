cf8728ec9b5679330db1c87f151365087c0b229d
Noah D. Ortiz <abstractednoah@brumal.org>
2022-07-20 16:48:05 -0700

    update alternatives

---
+++ share/update-alternatives.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+# set -x
+
+source "${BR_DOTFILES}/share/utilities.zsh" || exit -1
+
+_NIX_BIN="/nix/var/nix/profiles/per-user/abstractednoah/profile/bin"
+_ALT_BIN="/usr/bin"
+
+_nix_has() {
+    local _command="${1}"
+    test -x "${_NIX_BIN}/${_command}" || _br_oops "Missing: ${_command}"
+}
+
+_update_alternative() {
+    local _name="${1}"
+    local _command="${2}"
+    _nix_has "${_command}" || return
+    test -n "${_name}" \
+        || _br_oops "Alternative name empty" \
+        || return
+    update-alternatives --install \
+        "${_ALT_BIN}/${_name}" \
+        "${_name}" \
+        "${_NIX_BIN}/${_command}" \
+        0
+}
+
+_update_alternative editor vim
+_update_alternative x-www-browser qutebrowser
+_update_alternative gnome-www-browser qutebrowser
