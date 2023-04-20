8b3476d31aa44a5516b1d628ad218ed85ecd1367
Noah D. Ortiz <abstrnoah@brumal.org>
2023-04-20 16:27:09 -0700

    rm old rofi.sh

--- scripts-todo/rofi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-# This is a hacky wrapper that runs rofi with zsh rather than bash so that my
-# zshenv gets sourced correctly. TODO A more legit solution would be to setup
-# bash to emulate my zsh config so that I don't keep running into this issue.
-
-~/.nix-profile/bin/rofi "${@}"
