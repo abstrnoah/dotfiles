709a44606a557a9c3f4fcecd7f300be8a5b5e66f
Noah D. Ortiz <abstrnoah@brumal.net>
2024-04-22 20:59:51 -0700

    clean up config.nix

--- config.nix
+++ config.nix
@@ -2,7 +2,35 @@ inputs@{ flake-utils, nixpkgs, ... }:
 
 flake-utils.lib.eachDefaultSystem (system:
   let
-    config-system = {
+    this = {
+
+      username = "abstrnoah";
+      src-path = ./src;
+      dotfiles-source = "https://github.com/abstrnoah/dotfiles";
+      dotfiles-destination = "$HOME/.dotfiles";
+      machines.coyote = {
+        xrandr-outputs.builtin = "LVDS1";
+        xrandr-outputs.external = "VGA1";
+      };
+      systemd-user-units-path = "/home/me/.config/systemd/user";
+
+      nixpkgs-args = {
+        inherit system;
+        config = {
+          pulseaudio = true;
+          allowUnfreePredicate = p:
+            builtins.elem (this.get-name-substring p) [
+              "discord"
+              "spotify"
+              "spotify-unwrapped"
+              "vscode"
+              "xflux"
+              "zoom"
+              "slack"
+              "minecraft-launcher"
+            ];
+        };
+      };
 
       write-text = nixpkgs.nixpkgs.${system}.writeTextDir;
       write-shell-app = nixpkgs.nixpkgs.${system}.writeShellApplication;
@@ -16,47 +44,23 @@ flake-utils.lib.eachDefaultSystem (system:
       get-name-substring = nixpkgs.lib.strings.getName;
       path-append = nixpkgs.lib.path.append;
 
-      nixpkgs-args = {
-        inherit system;
-        config = {
-          pulseaudio = true;
-          allowUnfreePredicate = p:
-            builtins.elem (config-system.get-name-substring p) [
-              "discord"
-              "spotify"
-              "spotify-unwrapped"
-              "vscode"
-              "xflux"
-              "zoom"
-              "slack"
-              "minecraft-launcher"
-            ];
-        };
-      };
-
-      has-constructor-id = id: x:
-        x.${config-system.brumal-names.constructor}.${config-system.brumal-names.id} or null
-        == id;
-
       is-bundled = b: b ? brumal.bundle-packages;
 
       bundle-list-packages = b:
-        assert config-system.is-bundled b;
+        assert this.is-bundled b;
         builtins.attrValues b.brumal.bundle-packages;
 
       # flatten-bundles :: [ Package | Bundle ] -> [ Package ]
       # We operate on lists rather than sets to avoid overriding packages.
       flatten-bundles = ps:
         builtins.concatMap (p:
-          if config-system.is-bundled p then
-            config-system.flatten-bundles (config-system.bundle-list-packages p)
+          if this.is-bundled p then
+            this.flatten-bundles (this.bundle-list-packages p)
           else
             [ p ]) ps;
 
       bundle = { name, packages, args ? { } }:
-        let
-          packages' =
-            config-system.flatten-bundles (builtins.attrValues packages);
+        let packages' = this.flatten-bundles (builtins.attrValues packages);
         in nixpkgs.nixpkgs.${system}.buildEnv ({
           inherit name;
           paths = packages';
@@ -65,13 +69,8 @@ flake-utils.lib.eachDefaultSystem (system:
           brumal.bundle-packages = packages;
         };
 
-      username = "abstrnoah";
-
-      src-path = ./src;
-
-      dotfiles-source = "https://github.com/abstrnoah/dotfiles";
-      dotfiles-destination = "$HOME/.dotfiles";
-
+      # TODO `nix flake show` complains that this does not produce a derivation,
+      # although `nix flake build`ing works fine.
       store-source = {
         # Absolute path to the source, must not be a store path.
         source,
@@ -80,46 +79,41 @@ flake-utils.lib.eachDefaultSystem (system:
         assert builtins.typeOf source == "path";
         let
           name = baseNameOf source;
-          excludes' = map (p: config-system.path-append source p) excludes;
+          excludes' = map (p: this.path-append source p) excludes;
           filter = path: type: !builtins.elem (/. + path) excludes';
         in builtins.filterSource filter source;
 
+      # TODO take name instead of source path
       store-dotfiles = source:
-        config-system.store-source {
+        this.store-source {
           inherit source;
           excludes = [ "README.md" "default.nix" ];
         };
 
-      machines.coyote = {
-        xrandr-outputs.builtin = "LVDS1";
-        xrandr-outputs.external = "VGA1";
-      };
-
       call-with = args: f:
-        f
-        (config-system.get-attrs (builtins.attrNames (builtins.functionArgs f))
-          args);
+        f (this.get-attrs (builtins.attrNames (builtins.functionArgs f)) args);
+
+      # TODO call-with-many
 
       cons-package = config@{ ... }:
         packages@{ ... }:
         cons:
         config'@{ ... }:
         packages'@{ ... }:
-        config-system.call-with (packages // packages')
-        (config-system.call-with (config // config') cons);
+        this.call-with (packages // packages')
+        (this.call-with (config // config') cons);
 
       bundle-dotfiles = upstreams: name:
-        config-system.bundle {
+        this.bundle {
           inherit name;
           packages = {
             package = upstreams.${name};
-            rc = config-system.store-dotfiles
-              (config-system.path-append config-system.src-path name);
+            rc = this.store-dotfiles (this.path-append this.src-path name);
           };
         };
 
       store-symlink = name: source: destination:
-        config-system.store-symlinks {
+        this.store-symlinks {
           inherit name;
           mapping = [{ inherit source destination; }];
         };
@@ -127,18 +121,16 @@ flake-utils.lib.eachDefaultSystem (system:
       store-symlinks = { name, mapping }:
         let
           symlink-command = { source, destination }: ''
-            destination="$out"/${config-system.escape-shell-arg destination}
+            destination="$out"/${this.escape-shell-arg destination}
             mkdir -p "$(dirname "$destination")"
-            ln -s ${config-system.escape-shell-arg source} "$destination"
+            ln -s ${this.escape-shell-arg source} "$destination"
           '';
           commands = map symlink-command mapping;
-        in config-system.run-command-local name { } ''
+        in this.run-command-local name { } ''
           mkdir -p "$out"
           cd "$out"
-          ${config-system.concat-strings commands}
+          ${this.concat-strings commands}
         '';
 
-      systemd-user-units-path = "/home/me/.config/systemd/user";
-
     };
-  in { config = config-system; })
+  in { config = this; })
