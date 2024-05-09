2a46fef131f7b988a20f493a3d50c6c2265d77b9
Noah D. Ortiz <abstrnoah@brumal.net>
2024-05-08 22:27:54 -0700

    qutebrowser bind reverse cmd cycle

--- src/qutebrowser/home/me/.config/qutebrowser/config.py
+++ src/qutebrowser/home/me/.config/qutebrowser/config.py
@@ -163,11 +163,8 @@ config.bind(_leader + "s", "config-cycle -t scrolling.bar never always")
 
 # Command mode
 config.bind("<Ctrl-f>", "cmd-edit", mode="command")
-config.bind(
-    "<Ctrl-t>",
-    'spawn --userscript cycle-cmd-prefix :tab-select ":open -w" ":open -t"',
-    mode="command"
-)
+config.bind("<Ctrl-t>", 'spawn --userscript cycle-cmd-prefix :tab-select ":open -w" ":open -t" :open', mode="command")
+config.bind("<Ctrl-Shift+t>", 'spawn --userscript cycle-cmd-prefix ":open -t" ":open -w" :tab-select :open', mode="command")
 
 # javascript clipboard access
 config.bind("tsy", "config-cycle -p -t -u *://{url:host}/* content.javascript.clipboard none access ;; reload")
