cc4a2526c952f5dc277b43864bcd0e26413cd36c
Noah D. Ortiz <abstrnoah@brumal.net>
2024-11-15 15:53:08 -0500

    add xrandr shortcuts

--- src/xrandr/default.nix
+++ src/xrandr/default.nix
@@ -15,6 +15,32 @@ let
         feh --bg-fill ${wallpapers}/home/me/.wallpaper
       '';
     };
+  xrandr-switch-primary-left = name: primary: secondary:
+    write-shell-app {
+      name = "xrandr-switch-${name}";
+      runtimeInputs = [ xrandr feh ];
+      text = ''
+        xrandr \
+          --output ${escape-shell-arg primary} \
+          --auto --primary \
+          --output ${escape-shell-arg secondary} --auto --right-of ${escape-shell-arg primary} \
+          --dpi 100
+        feh --bg-fill ${wallpapers}/home/me/.wallpaper
+      '';
+    };
+  xrandr-switch-primary-right = name: primary: secondary:
+    write-shell-app {
+      name = "xrandr-switch-${name}";
+      runtimeInputs = [ xrandr feh ];
+      text = ''
+        xrandr \
+          --output ${escape-shell-arg primary} \
+          --auto --primary \
+          --output ${escape-shell-arg secondary} --auto --left-of ${escape-shell-arg primary} \
+          --dpi 100
+        feh --bg-fill ${wallpapers}/home/me/.wallpaper
+      '';
+    };
   builtin = machine.xrandr-outputs.builtin;
   external = machine.xrandr-outputs.external;
 in symlink-join {
@@ -22,5 +48,7 @@ in symlink-join {
   paths = [
     (xrandr-switch-output "builtin" builtin external)
     (xrandr-switch-output "external" external builtin)
+    (xrandr-switch-primary-right "builtin-external" external builtin)
+    (xrandr-switch-primary-left "external-builtin" external builtin)
   ];
 }
