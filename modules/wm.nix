88c855179beb0910f1936ab07f2f9356f38aec15
Noah D. Ortiz <abstrnoah@brumal.net>
2025-06-10 17:41:09 -0400

    migrate wallpaper

--- modules/wm.nix
+++ modules/wm.nix
@@ -1,7 +1,8 @@
-top@{ config, ... }:
+top@{ config, inputs, ... }:
 {
   flake.modules.brumal.wm =
     {
+      system,
       packages,
       library,
       lib,
@@ -22,12 +23,29 @@ top@{ config, ... }:
           text = ''exec "${packages.i3wm}/bin/i3"'';
           destination = "/home/me/.xsession";
         };
+
+        # TODO Replace with direct string interpolation of feh and i3lock commands.
+        wallpapers = library.storeSymlinks {
+          name = "wallpapers";
+          mapping = [
+            {
+              source = inputs.wallpapers.packages.${system}.pixel-city-at-night-png;
+              destination = "/home/me/.wallpaper";
+            }
+            {
+              source = inputs.wallpapers.packages.${system}.solarized-stars-png;
+              destination = "/home/me/.wallpaperlock";
+            }
+          ];
+        };
+
       };
 
       userPackages = {
         inherit (packages)
           i3wm
           i3status
+          # i3lock # TODO per distro address debian pam issue
           libnotify
           # wallpapers TODO
           wmctrl
