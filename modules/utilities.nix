c144003fd62dce101a115988cc99dfdabc385fb3
Noah D. Ortiz <abstrnoah@brumal.net>
2025-06-13 21:30:15 -0400

    library, utilities modules

---
+++ modules/utilities.nix
@@ -0,0 +1,30 @@
+{
+  library,
+  flake-parts-lib,
+  ...
+}:
+let
+  perSystemOutputModule = flake-parts-lib.mkTransposedPerSystemModule {
+    name = "utilities";
+    option = library.mkOption {
+      type = library.types.lazyAttrsOf library.types.anything;
+      default = { };
+      description = "utilities are not system-agnostic!";
+    };
+    file = ./utilities.nix;
+  };
+in
+{
+  config.perSystem =
+    { pkgs, ... }:
+    let
+      utilities = import ../utilities.nix {
+        inherit library;
+        nixpkgs = pkgs;
+      };
+    in
+    {
+      inherit utilities;
+      _module.args.utilities = utilities;
+    };
+}
