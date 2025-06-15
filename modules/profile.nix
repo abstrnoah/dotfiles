a955afd67bcb3af36cd7754b7fdb6af045be58e7
Noah D. Ortiz <abstrnoah@brumal.net>
2025-06-14 20:31:17 -0400

    module programs -> profile

--- modules/profile.nix
+++ modules/profile.nix
@@ -1,6 +1,6 @@
 # Inspired by https://github.com/lf-/flakey-profile
 {
-  flake.modules.brumal.programs =
+  flake.modules.brumal.profile =
     { library, utilities, config, ... }:
     let
       inherit (library)
