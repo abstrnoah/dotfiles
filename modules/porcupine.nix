ae98ff480bd24f844a529031934f7cdb80c3716b
Noah D. Ortiz <abstrnoah@brumal.net>
2025-10-15 12:34:19 -0400

    add hardware scan to porcupine

--- modules/porcupine.nix
+++ modules/porcupine.nix
@@ -1,7 +1,7 @@
 top@{ config, ... }:
 {
   flake.machineModules.porcupine =
-    { library, ... }:
+    { library, config, ... }:
     {
       networking.hostName = "porcupine";
       brumal.distro = "nixos";
@@ -10,5 +10,33 @@ top@{ config, ... }:
 
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
+
+      # From hardware scan:
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+      ];
+      boot.kernelModules = [ "kvm-intel" ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/b94f77a3-e5cf-42a2-92ca-b54e0565f248";
+        fsType = "ext4";
+      };
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/9CBC-B8E5";
+        fsType = "vfat";
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
+      };
+      swapDevices = [
+        { device = "/dev/disk/by-uuid/7c6b59c0-dedd-42d3-b174-e4a13231a3a8"; }
+      ];
+      networking.useDHCP = library.mkDefault true;
+      hardware.cpu.intel.updateMicrocode = library.mkDefault config.hardware.enableRedistributableFirmware;
+
     };
 }
