78106f1709a06ea8b71f9b56f4bae658b6297b85
abstrnoah <abstrnoah@brumal.net>
2025-10-18 16:59:02 +0000

    tools: emplacetree, htop

--- modules/tools.nix
+++ modules/tools.nix
@@ -5,12 +5,18 @@
 {
   flake.nixosModules.base = moduleWithSystem (
     perSystem@{ inputs' }:
-    { ... }:
+    { pkgs, ... }:
     {
       nixpkgs.overlays = [
         (final: prev: {
           emplacetree = inputs'.emplacetree.packages.emplacetree;
         })
+      ];
+      brumal.profile.packages = [
+        pkgs.emplacetree
+      ];
+      environment.systemPackages = [
+        pkgs.htop
       ];
     }
   );
