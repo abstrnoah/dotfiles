c3c32a2cdde85b3818ec863dcd731ade945826c0
Noah D. Ortiz <abstrnoah@brumal.net>
2025-10-18 11:59:32 -0400

    fmt

--- modules/env.nix
+++ modules/env.nix
@@ -6,7 +6,7 @@
       config.brumal.env = rec {
         HOME = "/home/${config.brumal.owner}";
         XDG_CONFIG_HOME = "${HOME}/.config";
-	NIX_PROFILE = "${HOME}/.nix-profile";
+        NIX_PROFILE = "${HOME}/.nix-profile";
       };
     };
 }
--- modules/vim.nix
+++ modules/vim.nix
@@ -11,7 +11,9 @@
       env = config.brumal.env;
       inherit (utilities) storeSymlink storeLegacyDotfiles;
       nvimAsVimP = storeSymlink "nvim-as-vim" "${pkgs.neovim}/bin/nvim" "/bin/vim";
-      vimPlugP = storeSymlink "vim-plug" "${pkgs.vimPlugins.vim-plug}/plug.vim" "${env.HOME}/.vim/autoload/plug.vim";
+      vimPlugP =
+        storeSymlink "vim-plug" "${pkgs.vimPlugins.vim-plug}/plug.vim"
+          "${env.HOME}/.vim/autoload/plug.vim";
       vimRcP = storeLegacyDotfiles "vim";
     in
     {
