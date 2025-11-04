02f0925eeb3ab75b65f7642a93037289831a16a9
abstrnoah <abstrnoah@brumal.net>
2025-11-04 22:23:52 +0000

    fix clipboard typo

--- modules/cfg/clipboard.nix
+++ modules/cfg/clipboard.nix
@@ -12,7 +12,7 @@
 
       brumal.programs.tmux.conf = ''
         bind -T copy-mode-vi v send-keys -X begin-selection
-        bind -T copy-mode-vi y send-keys -X copy-pipe "${cfg.yank-text}"
+        bind -T copy-mode-vi y send-keys -X copy-pipe "${cfg.yank}"
         bind C-p paste-buffer -p
       '';
     };
