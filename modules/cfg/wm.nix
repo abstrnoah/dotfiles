4cee98ff4dc1d9f297d6040f31e9f1b2a613602e
abstrnoah <abstrnoah@brumal.net>
2025-11-05 00:02:09 +0000

    i3 more navigation

--- modules/cfg/wm.nix
+++ modules/cfg/wm.nix
@@ -12,6 +12,7 @@ top@{ config, ... }:
       cfg = config.brumal.programs.i3wm;
       k = cfg.keys;
       env = config.brumal.env;
+      dims = library.mapAttrs (_: builtins.toString) cfg.dimensions;
     in
     {
 
@@ -54,22 +55,32 @@ top@{ config, ... }:
           default_border = 1;
           base_gap_inner = 7;
           base_gap_outer = 0;
+          resize_sm = 2;
+          resize_lg = 50;
         };
 
         brightness_interval = 5;
 
         body.directives = [
           ''border_radius 2''
-          ''default_border pixel ${builtins.toString cfg.dimensions.default_border}''
-          ''gaps inner ${builtins.toString cfg.dimensions.base_gap_inner}''
+          ''default_border pixel ${dims.default_border}''
+          ''gaps inner ${dims.base_gap_inner}''
           # Use Mouse+$mod to drag floating windows to their wanted position
           ''floating_modifier ${k.mod}''
+          ''show_marks no''
         ];
 
         body.leader = k.mod;
 
         body.bindsym = {
+          # Terminal
           "${k.enter}" = "exec i3-sensible-terminal";
+
+          # Run
+          ${k.colon} = "exec rofi -show run";
+
+          # File browser
+          slash = "exec rofi -show filebrowser";
 
           # Change focus
           h = "focus left";
@@ -89,7 +100,7 @@ top@{ config, ... }:
 
           # Change container layout
           t = "layout toggle tabbed stacking";
-          e = "layout toggle tabbed stacking";
+          e = "layout toggle splitv splith";
 
           # Kill focused window
           "${k.shift}+q" = "kill";
@@ -97,16 +108,42 @@ top@{ config, ... }:
           # Fullscreen focused container
           f = "fullscreen toggle";
 
+          # Floating
+          "${k.shift}+space" = "floating toggle";
+
+          # Marks
+          m = "exec i3-input -F 'mark --add %s' -l 1 -P 'mark: '";
+          ${k.singlequote} = "exec i3-input -F '[con_mark=\"^%s$\"] focus' -l 1 -P 'goto: '";
+          "${k.shift}+m" = "unmark";
+
+          # Go to window
+          d = "exec rofi -show window";
+
+          # Change workspace
+          n = "workspace next_on_output";
+          p = "workspace prev_on_output";
+          comma = "workspace back_and_forth";
+          "${k.alt}+r" = "exec i3-input -F 'rename workspace to \"%s\"' -P 'rename ws: '";
         };
 
         body.modes.system = {
-          key = "${k.mod}+${k.grave}";
+          key = "${k.grave}";
           block.body.bindsym = {
             e = "exec i3-nagbar -t warning -m 'Exit i3wm?' -b 'Yeah.' 'i3-msg exit'";
             s = "exec i3-nagbar -t warning -m 'Shutdown?' -b 'Yeah.' 'shutdown now'";
             "${k.shift}+s" = "exec i3-nagbar -t warning -m 'Reboot?' -b 'Yeah.' 'shutdown -r now'";
             r = "reload";
             "${k.shift}+r" = "restart";
+          };
+        };
+
+        body.modes.resize = {
+          key = "r";
+          block.body.bindsym = {
+            h = "resize shrink width ${dims.resize_sm} px or ${dims.resize_sm} ppt";
+            j = "resize grow height ${dims.resize_sm} px or ${dims.resize_sm} ppt";
+            k = "resize shrink height ${dims.resize_sm} px or ${dims.resize_sm} ppt";
+            l = "resize grow width ${dims.resize_sm} px or ${dims.resize_sm} ppt";
           };
         };
 
