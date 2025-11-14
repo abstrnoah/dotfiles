52ebb88deae3a4f2346df5caa59f4a0728e3fd5a
abstrnoah <abstrnoah@brumal.net>
2025-11-14 04:44:58 +0000

    input touchpad disable and disable while typing

--- modules/cfg/input.nix
+++ modules/cfg/input.nix
@@ -1,6 +1,17 @@
 {
-  flake.nixosModules.base = {
-    services.xserver.xkb.options = "ctrl:nocaps";
-    console.useXkbConfig = true;
-  };
+  flake.nixosModules.base =
+    { pkgs, ... }:
+    let
+      trackpointDeviceName = "TPPS/2 IBM TrackPoint";
+      touchpadDeviceName = "Synaptics TM3276-022";
+    in
+    {
+      services.xserver.xkb.options = "ctrl:nocaps";
+      console.useXkbConfig = true;
+      # https://github.com/NixOS/nixpkgs/issues/299468
+      services.xserver.displayManager.sessionCommands = ''
+        ${pkgs.xorg.xinput}/bin/xinput set-prop "${touchpadDeviceName}" "libinput Disable While Typing Enabled" 1
+        ${pkgs.xorg.xinput}/bin/xinput disable "${touchpadDeviceName}"
+      '';
+    };
 }
