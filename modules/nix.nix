f9bb8edfe666af9230597404b43a88db00cfa041
abstrnoah <abstrnoah@brumal.net>
2025-11-22 23:09:43 +0000

    nix add forester substituter

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,5 +6,14 @@
       "pipe-operators"
     ];
     allow-import-from-derivation = true;
+    trusted-users = [
+      "@wheel"
+    ];
+    extra-substituters = [
+      "https://forester.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "forester.cachix.org-1:pErGVVci7kZWxxcbQ/To8Lvqp6nVTeyPf0efJxbrQDM="
+    ];
   };
 }
