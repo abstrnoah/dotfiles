6d97ca9307a2ac1c7d6dbfd313c5d1f0925f79a0
abstrnoah <abstrnoah@brumal.net>
2025-11-22 19:46:52 +0000

    start setting up audio

---
+++ modules/audio.nix
@@ -0,0 +1,9 @@
+{
+  flake.nixosModules.base = {pkgs, ...}: 
+  {
+    security.rtkit.enable = true;
+    services.pipewire.enable = true;
+    brumal.nixpkgs.allowUnfree = [ "spotify" ];
+    brumal.profile.packages = [ pkgs.spotify ];
+  };
+}
