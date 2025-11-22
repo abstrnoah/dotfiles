bf83d849991293a5681b664a2b7c47ae8731f91c
abstrnoah <abstrnoah@brumal.net>
2025-11-22 22:20:29 +0000

    ssh add tipu.local

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -9,6 +9,10 @@
           Port 2202
           ProxyCommand ${pkgs.netcat}/bin/nc -X connect -x %h:443 %h %p
           User abstractednoah
+        Host tipu.local
+          HostName tipu.local
+          Port 2202
+          User abstractednoah
       '';
     };
 }
