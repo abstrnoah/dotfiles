dee3db40a898bacef4ba0c747deb27c068645761
abstrnoah <abstrnoah@brumal.net>
2025-11-22 22:20:03 +0000

    networking add avahi and mullvad

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,8 +1,15 @@
 {
   flake.nixosModules.base =
-    { config, ownerName, ... }:
+    { config, ownerName, pkgs, ... }:
     {
+      environment.systemPackages = [ pkgs.dig ];
       networking.networkmanager.enable = true;
       users.users.${ownerName}.extraGroups = [ "networkmanager" ];
+      services.mullvad-vpn.enable = true;
+      services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        nssmdns6 = true;
+      };
     };
 }
