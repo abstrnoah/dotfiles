e634831497d0e3cecf73a35f40a80e5663ba7cac
abstrnoah <abstrnoah@brumal.net>
2025-11-22 19:18:57 +0000

    i3 bind suspend-then-hibernate

--- modules/power.nix
+++ modules/power.nix
@@ -1,6 +1,26 @@
 {
-  flake.nixosModules.base = {
-    services.logind.settings.Login.HanldeLidSwitch = "ignore";
-    programs.xss-lock.enable = true;
-  };
+  flake.nixosModules.gui =
+    { config, ... }:
+    let
+      cfg = config.brumal.i3wm;
+      k = cfg.keys;
+    in
+    {
+      services.logind.settings.Login.IdleAction = "suspend-then-hibernate";
+      # TODO Doesn't seem to work.
+      services.logind.settings.Login.IdleActionSec = "1min";
+      programs.xss-lock.enable = true;
+
+      brumal.i3wm.body.bindsym.${k.tilde} = "exec systemctl suspend-then-hibernate";
+      brumal.i3wm.body.modes.system = {
+        key = "${k.grave}";
+        block.body.bindsym = {
+          e = "exec i3-nagbar -t warning -m 'Exit i3wm?' -b 'Yeah.' 'i3-msg exit'";
+          s = "exec i3-nagbar -t warning -m 'Shutdown?' -b 'Yeah.' 'shutdown now'";
+          "${k.shift}+s" = "exec i3-nagbar -t warning -m 'Reboot?' -b 'Yeah.' 'shutdown -r now'";
+          r = "reload";
+          "${k.shift}+r" = "restart";
+        };
+      };
+    };
 }
