e10b6f1fd976341e6cd1ba271a226ab9cc3bfdbe
abstrnoah <abstrnoah@brumal.net>
2025-11-23 16:51:58 -0500

    sleep-then-hibernate working as desired

--- modules/power.nix
+++ modules/power.nix
@@ -4,14 +4,26 @@
     let
       cfg = config.brumal.i3wm;
       k = cfg.keys;
+      sleep-operation = "suspend-then-hibernate";
     in
     {
-      services.logind.settings.Login.IdleAction = "suspend-then-hibernate";
-      # TODO Doesn't seem to work.
-      services.logind.settings.Login.IdleActionSec = "1min";
       programs.xss-lock.enable = true;
+      services.logind.settings.Login.HandleLidSwitch = sleep-operation;
+      services.logind.settings.Login.HandlePowerKey = sleep-operation;
+      services.logind.settings.Login.HandleSuspendKey = sleep-operation;
+      services.logind.settings.Login.IdleAction = sleep-operation;
+      systemd.sleep.extraConfig = ''HibernateDelaySec=30min'';
+      # TODO Doesn't seem to work.
+      services.logind.settings.Login.IdleActionSec = "5min";
 
-      brumal.i3wm.body.bindsym.${k.tilde} = "exec systemctl suspend-then-hibernate";
+      systemd.services.no-wakeup-on-lid = {
+        # TODO This is a toggle so we should really verify first that it's enabled.
+        script = ''echo LID > /proc/acpi/wakeup'';
+        wantedBy = [ "multi-user.target" ];
+      };
+
+      # Unneeded because HandlePowerKey
+      # brumal.i3wm.body.bindsym.${k.tilde} = "exec systemctl suspend-then-hibernate";
       brumal.i3wm.body.modes.system = {
         key = "${k.grave}";
         block.body.bindsym = {
