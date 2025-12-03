62ca7a070a422f2e3cd2f18435e5bed298125c38
abstrnoah <abstrnoah@brumal.net>
2025-12-03 14:55:02 -0500

    enable fzf in shell

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,22 +1,27 @@
 {
-  flake.nixosModules.base.brumal.bash = {
-    inputrc = ''
-      set bell-style none
-      set editing-mode vi
-      set keymap vi
-    '';
-    profile = ''
-      ttdl-all() {
-          ttdl list --all --completed none "$@"
-      }
+  flake.nixosModules.base = {
+    brumal.bash = {
+      inputrc = ''
+        set bell-style none
+        set editing-mode vi
+        set keymap vi
+      '';
+      profile = ''
+        ttdl-all() {
+            ttdl list --all --completed none "$@"
+        }
 
-      ttdl-unsorted() {
-          ttdl-all --pri none "$@"
-      }
+        ttdl-unsorted() {
+            ttdl-all --pri none "$@"
+        }
 
-      ttdl-now() {
-          ttdl list --pri x+ "$@"
-      }
-    '';
+        ttdl-now() {
+            ttdl list --pri x+ "$@"
+        }
+      '';
+
+    };
+    programs.fzf.keybindings = true;
+    programs.fzf.fuzzyCompletion = true;
   };
 }
