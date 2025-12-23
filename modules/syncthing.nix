f9c9bd8081392d649851129d9eba65e828def62e
abstrnoah <abstrnoah@brumal.net>
2025-12-22 18:47:21 -0700

    syncthing back to imperative install for now
    This solves issue where syncthing is started automatically by lightdm
    which conflicts with my syncthing.

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -9,12 +9,10 @@
       inherit (library) readFile;
     in
     {
-      # FIXME failing to start at boot due to reported port conflict
       # TODO make syncthing config declarative
       systemd.user.units."syncthing.service" = {
         text = readFile "${pkgs.syncthing}/share/systemd/user/syncthing.service";
-        # Turns out systemd ignores [Install] section; we must enable directly.
-        wantedBy = [ "default.target" ];
+        # DO NOT set wantedBy = [ "default.target" ]; because that cuases every user (including eg lightdm) to try to spin up syncthing. Instead we need a better systemd user setup. For now I'm imperatively `systemctl --user enable syncthing` ewww.
       };
     };
 }
