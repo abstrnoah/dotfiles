c64195e47cfb059b536c780e1e67a6f6b75f1acf
abstrnoah <abstrnoah@brumal.net>
2025-12-26 20:36:02 -0700

    input refactor porcupine

--- modules/input.nix
+++ modules/input.nix
@@ -1,13 +1,17 @@
 {
   flake.nixosModules.base =
     { pkgs, ... }:
+    {
+      services.xserver.xkb.options = "ctrl:nocaps";
+      console.useXkbConfig = true;
+    };
+  flake.machineModules.porcupine =
+    { pkgs, ... }:
     let
       trackpointDeviceName = "TPPS/2 IBM TrackPoint";
       touchpadDeviceName = "Synaptics TM3276-022";
     in
     {
-      services.xserver.xkb.options = "ctrl:nocaps";
-      console.useXkbConfig = true;
       # https://github.com/NixOS/nixpkgs/issues/299468
       services.xserver.displayManager.sessionCommands = ''
         ${pkgs.xorg.xinput}/bin/xinput set-prop "${touchpadDeviceName}" "libinput Disable While Typing Enabled" 1
