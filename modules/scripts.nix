0241b1d5e77e1be47a77f0fd20b6d5f92849a2d2
abstrnoah <abstrnoah@brumal.net>
2026-01-02 13:37:51 -0500

    watchdumb

---
+++ modules/scripts.nix
@@ -0,0 +1,22 @@
+{
+  flake.nixosModules.base =
+    { pkgs, ... }:
+    {
+      brumal.files.bin = {
+        watchdumb.text = ''
+          if [ "$#" -lt 2 ]; then
+            exit 1
+          fi
+          interval="$1"
+          shift
+          while true; do
+            clear
+            date +'[%a %Y-%m-%d %H:%M:%S %Z]'
+            echo "> $*"
+            "$@"
+            sleep "$interval"
+          done
+        '';
+      };
+    };
+}
