48148a9e582e8f92de51b43948b7cc3efb3ba311
abstrnoah <abstrnoah@brumal.net>
2026-01-15 16:44:49 -0500

    enable wacom for xsetwacom

--- modules/input.nix
+++ modules/input.nix
@@ -10,11 +10,14 @@
     let
       trackpointDeviceName = "TPPS/2 IBM TrackPoint";
       touchpadDeviceName = "Synaptics TM3276-022";
+      wacomDeviceName = "Wacom One by Wacom S Pen Pen (0)";
     in
     {
       # https://github.com/NixOS/nixpkgs/issues/299468
       services.xserver.displayManager.sessionCommands = ''
         ${pkgs.xorg.xinput}/bin/xinput set-prop "${touchpadDeviceName}" "libinput Disable While Typing Enabled" 1
       '';
+      # TODO Automatically map tablet to primary output
+      services.xserver.wacom.enable = true;
     };
 }
